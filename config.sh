819f34d8c524b4868b197b4943f5f2e22ab8b8e1
Rishabh <rishabhad93@gmail.com>
2016-02-27 21:52:04 +0530

    add ferrari

--- config.sh
+++ config.sh
@@ -153,6 +153,11 @@ case "$1" in
 	repo_sync yu
 	;;
 
++"ferrari-l-cm")
+ +	echo PRODUCT_NAME=$1 | sed 's/.....$//' >> .tmp-config &&
+ +	repo_sync ferrari
+ +	;;
+ +
 "d802-l-cm"|"d855-l-cm")
 	echo PRODUCT_NAME=full_$1 | sed 's/.....$//' >> .tmp-config &&
 	repo_sync lge
